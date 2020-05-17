7ba52846cd9011d0cc79aaf3b74eb0543af371d6
gitaanjali <gitaanjalide@gmail.com>
2020-05-17 23:38:34 +0530

    update code flipcoin

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,2 +1,18 @@
 #!/bin/bash -x
-
+read -p "Enter Times to flip: " num
+heads=0
+tails=0
+count=0
+while [ $count -ne $num ]
+do
+	$((count++))
+	result=$((RANDOM % 2))
+	if [ $result -eq 0 ]
+	then
+		$((tails++))
+	else
+		$((heads++))
+	fi
+done
+echo "Heads: " $heads
+echo "Tails: " $tails
