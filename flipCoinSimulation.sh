dbe0c104b75e683ca22cd6a449b91af522e28cec
gitanjali-13 <gitaanjalide@gmail.com>
2020-05-17 23:19:53 +0530

    Matches is either tie or win

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,4 +1,26 @@
 #!/bin/bash -x
 
-echo "Welcome"
+heads=0
+tails=0
+while [ $tails -le 21 -o $heads -le 21 ]
+do
+	result=$((RANDOM % 2))
+	if [ $result -eq 0 ]
+	then
+		$((tails++))
+	else
+		$((heads++))
+	fi
+done
+
+if [$heads -eq $tails ]
+then
+	echo "It's  TIE !!!"
+elif [ $heads -gt $tails ]
+then
+	echo "Heads Wins..."
+elif [ $heads -lt $tails ]
+then
+	echo "Tails Wins..."
+fi
 
