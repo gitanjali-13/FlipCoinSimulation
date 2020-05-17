084f2a7748ea727ecb9a886bc882e18361cacfed
gitanjali-13 <gitaanjalide@gmail.com>
2020-05-17 23:56:11 +0530

    flip coin

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,28 +1,32 @@
 #!/bin/bash -x
-
-HEAD
 heads=0
 tails=0
+
+flipCoin() {
+	result=$((RANDOM % 2))
+        if [ $result -eq 0 ]
+        then
+                $((tails++))
+        else
+                $((heads++))
+        fi
+}
+
 while [ $tails -le 21 -o $heads -le 21 ]
 do
-	result=$((RANDOM % 2))
-	if [ $result -eq 0 ]
-	then
-		$((tails++))
-	else
-		$((heads++))
-	fi
+	flipCoin
 done
 
 if [$heads -eq $tails ]
 then
-	echo "It's  TIE !!!"
+	while [ $(($heads - $tails)) -ne 2 -o $(($tails - $heads)) -ne 2 ]
+	do
+		flipCoin
+	done
 elif [ $heads -gt $tails ]
 then
-	echo "Heads Wins..."
+	echo "Heads Wins !"
 elif [ $heads -lt $tails ]
 then
-	echo "Tails Wins..."
+	echo "Tails Wins !"
 fi
-
-
