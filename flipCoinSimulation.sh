d5b3bdb5c89ecc0d38ffccb3f864ce68d4e02b6d
gitanjali-13 <gitaanjalide@gmail.com>
2020-05-17 23:00:38 +0530

    No of times coin flip

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,4 +1,22 @@
 #!/bin/bash -x
 
-echo "Welcome"
+read -p "Enter no of times to flip coin:" num
+heads=0
+tails=0
+count=0
+
+while [ $count -ne $num ]
+do
+	$((count++))
+	result=$((RANDOM%2))
+	if [ $result -eq 0 ]
+	then
+		$((tails++))
+	else
+		$((heads++))
+	fi
+done
+echo "Heads:" $heads
+echo "Tails:" $tails
+
 
