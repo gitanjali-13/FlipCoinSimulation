02cfb99c13dbeca253e928bd6db34c10ad262cc7
gitanjali-13 <gitaanjalide@gmail.com>
2020-05-17 23:46:28 +0530

    If match tie then Game continues...

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,28 +1,31 @@
 #!/bin/bash -x
-
-HEAD
 heads=0
 tails=0
-while [ $tails -le 21 -o $heads -le 21 ]
-do
-	result=$((RANDOM % 2))
+flipCoin() {
+	result=$((RANDOM%2))
 	if [ $result -eq 0 ]
 	then
 		$((tails++))
 	else
 		$((heads++))
 	fi
+}
+
+while [ $tails -le 21 -o $heads -le 21 ]
+do
+	flipCoin
 done
 
-if [$heads -eq $tails ]
-then
-	echo "It's  TIE !!!"
+if [ $heads -eq $tails ]
+	then
+	while [ $(($heads - $tails)) -ne 2 -o $(($tails - $heads)) -ne 2 ]
+	do
+		flipCoin
+	done
 elif [ $heads -gt $tails ]
 then
-	echo "Heads Wins..."
+	echo "Heads Wins !"
 elif [ $heads -lt $tails ]
 then
-	echo "Tails Wins..."
+	echo "Tails Wins !"
 fi
-
-
